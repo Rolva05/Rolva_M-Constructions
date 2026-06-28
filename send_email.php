ad63df2ddaa2a009522e3cf29180392c885fdb13
Rolva05 <Kamogelorolva03@gmail.com>
2026-06-28 20:40:17 +0200

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,7 @@
+<?php
+$to="yourbusiness@example.com";
+$name=$_POST['name'];$email=$_POST['email'];$subject=$_POST['subject'];$message=$_POST['message'];
+$body="Name: $name\nEmail: $email\n\n$message";
+$headers="From: $email";
+if(mail($to,$subject,$body,$headers)){echo "Enquiry sent successfully.";}else{echo "Failed to send enquiry.";}
+?>
